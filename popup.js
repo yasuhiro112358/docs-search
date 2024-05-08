3c58734ce2fe12accc335c6fbfbf9cef6c1330c9
Yasuhiro WATANABE <info@nabeyasu.com>
2024-05-08 19:14:33 +0900

    made release v1.0

--- popup.js
+++ popup.js
@@ -1,24 +1,18 @@
 'use strict';
 
 {
-  document.addEventListener('DOMContentLoaded', () => {
-    let btn_search = document.querySelector('#btn-search');
-
-    btn_search.addEventListener('click', () => {
-      search();
-    });
-  });
-
-  // function search(search_query) {
+  // Functions
   function search() {
-    // let query = document.getElementById('searchQuery').value;
     let query = document.querySelector('#search-query').value;
     let search_query = 'https://www.google.com/search?q=';
 
     const official_docs = {
-      "html-css-js": "https://developer.mozilla.org/en-US/",
+      "html-css-js": "https://developer.mozilla.org/",
       "bootstrap-5": "https://getbootstrap.com/docs/5.3/",
-      "php": "https://www.php.net/manual/en/",
+      "php": "https://www.php.net/",
+      "laravel-11": "https://laravel.com/docs/11.x/",
+      "python": "https://docs.python.org/",
+      "git": "https://git-scm.com/",
     };
 
     const selected_docs = [];
@@ -29,14 +23,10 @@
       if (tech_checkbox.checked) {
         selected_docs.push(`site:${official_docs[tech]}`);
       }
-
-      console.log(selected_docs);
     });
 
     // Generate search query
-    if (selected_docs.length >= 2) {
-      search_query += "(" + selected_docs.join(" OR ") + ") " + encodeURIComponent(query);
-    } else if (selected_docs.length == 1) {
+    if (selected_docs.length >= 1) {
       search_query += selected_docs.join(" OR ") + " " + encodeURIComponent(query);
     } else {
       search_query += encodeURIComponent(query);
@@ -44,8 +34,32 @@
 
     chrome.tabs.create({ url: search_query });
   }
+
+
+
+  // ==== Main ====
+  document.addEventListener('DOMContentLoaded', () => {
+    const btn_search = document.querySelector('#btn-search');
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+    // Search Start
+    btn_search.addEventListener('click', () => {
+      search();
+    });
+
+    // Save and Load status of Checkboxes
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener("change", () => {
+        // Save status
+        chrome.storage.local.set({ [checkbox.id]: checkbox.checked });
+      })
+
+      // Load status
+      chrome.storage.local.get([checkbox.id], (result) => {
+        checkbox.checked = result[checkbox.id] || false;
+      });
+    });
+  });
+
+
 }
-
-
-
-
