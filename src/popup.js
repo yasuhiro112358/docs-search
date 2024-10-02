cccbbea5fb4c100fef1353dc64b29cfc33571508
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-02 23:28:04 +0900

    Refactor CSS styles: Add new languages and libraries to popup.js

--- src/popup.js
+++ src/popup.js
@@ -9,23 +9,15 @@
       url: "https://developer.mozilla.org/",
       color: "#040404",
       textColor: "white",
-      group: "FE Dev"
+      group: "Languages"
     },
     {
-      id: "bootstrap-5",
-      labels: ["Bootstrap 5"],
-      url: "https://getbootstrap.com/docs/5.3/",
-      color: "#6F2CF4",
+      id: "typescript",
+      labels: ["TypeScript"],
+      url: "https://www.typescriptlang.org/docs/",
+      color: "#3178C6",
       textColor: "white",
-      group: "FE Dev"
-    },
-    {
-      id: "chart-js",
-      labels: ["Chart.js"],
-      url: "https://www.chartjs.org/",
-      color: "#FF787C",
-      textColor: "white",
-      group: "FE Dev"
+      group: "Languages"
     },
     {
       id: "php",
@@ -33,7 +25,73 @@
       url: "https://www.php.net/",
       color: "#7986B8",
       textColor: "white",
-      group: "BE Dev"
+      group: "Languages"
+    },
+    {
+      id: "python",
+      labels: ["Python"],
+      url: "https://docs.python.org/",
+      color: "#3772A2",
+      // textColor: "#3772A2", 
+      textColor: "white",
+      backgroundColor: "#FFDF5B",
+      group: "Languages"
+    },
+    {
+      id: "bootstrap-5",
+      labels: ["Bootstrap 5"],
+      url: "https://getbootstrap.com/docs/5.3/",
+      color: "#6F2CF4",
+      textColor: "white",
+      group: "Libraries"
+    },
+    {
+      id: "tailwind-css",
+      labels: ["Tailwind CSS"],
+      url: "https://tailwindcss.com/docs/",
+      color: "#37BCF8",
+      textColor: "white",
+      group: "Libraries"
+    },
+    {
+      id: "jquery",
+      labels: ["jQuery"],
+      url: "https://api.jquery.com/",
+      color: "#0869AD",
+      textColor: "white",
+      group: "Libraries"
+    },
+    {
+      id: "react",
+      labels: ["React"],
+      url: "https://react.dev/",
+      color: "#58C4DC",
+      textColor: "white",
+      group: "Libraries"
+    },
+    {
+      id: "chart-js",
+      labels: ["Chart.js"],
+      url: "https://www.chartjs.org/",
+      color: "#FF787C",
+      textColor: "white",
+      group: "Libraries"
+    },
+    {
+      id: "d3-js",
+      labels: ["D3.js"],
+      url: "https://d3js.org/",
+      color: "#EF7234",
+      textColor: "white",
+      group: "Libraries"
+    },
+    {
+      id: "three-js",
+      labels: ["Three.js"],
+      url: "https://threejs.org/docs/",
+      color: "black",
+      textColor: "white",
+      group: "Libraries"
     },
     {
       id: "laravel-11",
@@ -41,40 +99,15 @@
       url: "https://laravel.com/docs/11.x/",
       color: "#EF3D2E",
       textColor: "white",
-      group: "BE Dev"
+      group: "Frameworks, CMSs and Databases"
     },
     {
-      id: "node-js",
-      labels: ["Node.js"],
-      url: "https://nodejs.org/",
-      color: "#54A345",
+      id: "wordpress",
+      labels: ["WordPress"],
+      url: "https://wordpress.org/documentation/",
+      color: "#01759B",
       textColor: "white",
-      group: "BE Dev"
-    },
-    {
-      id: "npm",
-      labels: ["npm"],
-      url: "https://docs.npmjs.com/",
-      color: "#CB0001",
-      textColor: "white",
-      group: "BE Dev"
-    },
-    {
-      id: "vite",
-      labels: ["Vite"],
-      url: "https://vitejs.dev/",
-      color: "#636CFF",
-      textColor: "white",
-      group: "BE Dev"
-    },
-    {
-      id: "python",
-      labels: ["Python"],
-      url: "https://docs.python.org/",
-      color: "#3772A2",
-      textColor: "#3772A2",
-      backgroundColor: "#FFDF5B",
-      group: "BE Dev"
+      group: "Frameworks, CMSs and Databases"
     },
     {
       id: "mysql",
@@ -82,7 +115,7 @@
       url: "https://dev.mysql.com/",
       color: "#01758F",
       textColor: "white",
-      group: "BE Dev"
+      group: "Frameworks, CMSs and Databases"
     },
     {
       id: "openai",
@@ -90,15 +123,55 @@
       url: "https://platform.openai.com/",
       color: "#000000",
       textColor: "white",
-      group: "BE Dev"
+      group: "APIs"
+    },
+    {
+      id: "google-cloud",
+      labels: ["Google Cloud"],
+      url: "https://cloud.google.com/docs",
+      color: "#007DF6",
+      textColor: "white",
+      group: "APIs"
+    },
+    {
+      id: "openweathermap",
+      labels: ["OpenWeatherMap"],
+      url: "https://openweathermap.org/",
+      color: "#EB6E4A",
+      textColor: "white",
+      group: "APIs"
+    },
+    {
+      id: "node-js",
+      labels: ["Node.js"],
+      url: "https://nodejs.org/",
+      color: "#54A345",
+      textColor: "white",
+      group: "Envs & Tools"
+    },
+    {
+      id: "npm",
+      labels: ["npm"],
+      url: "https://docs.npmjs.com/",
+      color: "#CB0001",
+      textColor: "white",
+      group: "Envs & Tools"
+    },
+    {
+      id: "vite",
+      labels: ["Vite"],
+      url: "https://vitejs.dev/",
+      color: "#636CFF",
+      textColor: "white",
+      group: "Envs & Tools"
     },
     {
       id: "docker",
-      labels: ["docker"],
+      labels: ["Docker"],
       url: "https://docs.docker.com/",
       color: "#1C63ED",
       textColor: "white",
-      group: "etc"
+      group: "Envs & Tools"
     },
     {
       id: "mamp",
@@ -106,16 +179,17 @@
       url: "https://documentation.mamp.info/",
       color: "#F66826",
       textColor: "white",
-      group: "etc"
+      group: "Envs & Tools"
     },
     {
       id: "git",
-      labels: ["git"],
+      labels: ["Git"],
       url: "https://git-scm.com/",
       color: "#F54D27",
       textColor: "white",
-      group: "etc"
-    }
+      group: "Envs & Tools"
+    },
+
   ];
 
   // サイトのチェックボックスを生成
@@ -136,17 +210,17 @@
     Object.keys(groups).forEach(groupName => {
       // グループコンテナを作成
       const groupDiv = document.createElement('div');
-      groupDiv.classList.add('mb-4');
+      groupDiv.classList.add('mb-2');
 
       // グループ見出しを追加
       const h2 = document.createElement('h2');
-      h2.classList.add('text-sm', 'text-gray-500', 'text-center', 'mb-2');
+      h2.classList.add('text-xs', 'text-gray-500', 'text-center', 'mb-1');
       h2.textContent = groupName;
       groupDiv.appendChild(h2);
 
       // チェックボックスのコンテナを作成
       const checkboxContainer = document.createElement('div');
-      checkboxContainer.classList.add('flex', 'flex-wrap', 'justify-center');
+      checkboxContainer.classList.add('flex', 'flex-wrap', 'justify-left', 'gap-1');
 
       groups[groupName].forEach(site => {
         // チェックボックスとラベルを作成
@@ -159,7 +233,7 @@
         input.value = site.id;
 
         const label = document.createElement('label');
-        label.classList.add('px-3', 'py-1', 'border', 'rounded', 'm-1', 'cursor-pointer', 'text-sm');
+        label.classList.add('px-4', 'py-2', 'border-2', 'rounded-full', 'cursor-pointer', 'text-md', 'select-none', 'block');
         label.setAttribute('for', site.id);
 
         // スタイルを適用
@@ -230,6 +304,18 @@
       search();
     });
 
+    // Enterキーで検索
+    const searchQuery = document.getElementById('search-query');
+    searchQuery.addEventListener('keydown', function (event) {
+      // Check if the 'Enter' key is pressed
+      if (event.key === 'Enter') {
+        // Prevent the default action (form submission, etc.)
+        event.preventDefault();
+        // Trigger the button click
+        document.getElementById('btn-search').click();
+      }
+    });
+
     // チェックボックスの状態を保存・復元
     checkboxes.forEach(checkbox => {
       checkbox.addEventListener("change", () => {
@@ -240,6 +326,21 @@
       // 状態を復元
       chrome.storage.local.get([checkbox.id], (result) => {
         checkbox.checked = result[checkbox.id] || false;
+        // チェック状態に応じてスタイルを適用
+        checkbox.dispatchEvent(new Event('change'));
+      });
+    });
+
+    // リセットボタンにイベントリスナーを追加
+    document.getElementById('btn-reset').addEventListener('click', function () {
+      // フォーム内の全てのチェックボックスを取得
+      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+      // 各チェックボックスを未チェックにする
+      checkboxes.forEach(function (checkbox) {
+        checkbox.checked = false;
+        // 状態を保存
+        chrome.storage.local.set({ [checkbox.id]: checkbox.checked });
         // チェック状態に応じてスタイルを適用
         checkbox.dispatchEvent(new Event('change'));
       });
